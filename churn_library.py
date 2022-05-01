da3d07453f4043e946cd3d21e6a2a6f20c2f7c1e
sarthak dargan <sarthak221995@gmail.com>
2022-05-01 10:50:17 +0530

    project structure

---
+++ churn_library.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May  1 10:43:42 2022
+
+@author: sarthak dargan sarthak221995@gmail.com
+"""
+
---
+++ churn_script_logging_and_tests.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May  1 10:47:22 2022
+
+@author: sarthak
+"""
+
