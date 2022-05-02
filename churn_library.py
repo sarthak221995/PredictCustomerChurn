0164969154f2acc70a15d818925658c923ce77d0
sarthak dargan <sarthak221995@gmail.com>
2022-05-02 22:15:59 +0530

    Submission v1, churn library.py and churn_script_logging_and_tests.py

--- churn_library.py
+++ churn_library.py
@@ -4,19 +4,18 @@ Created on Sun May  1 10:43:42 2022
 
 @author: sarthak dargan sarthak221995@gmail.com
 """
-from sklearn.metrics import plot_roc_curve, classification_report
-from sklearn.model_selection import GridSearchCV
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.linear_model import LogisticRegression
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import normalize
+
 import os
-import shap
 import joblib
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+from sklearn.metrics import plot_roc_curve, classification_report
+from sklearn.model_selection import GridSearchCV
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
 sns.set()
 
 os.environ['QT_QPA_PLATFORM'] = 'offscreen'
@@ -31,11 +30,10 @@ def import_data(pth):
     output:
             df: pandas dataframe
     '''
-    df = pd.read_csv(pth)
-    print(df.columns)
-    df['Churn'] = df.Attrition_Flag.apply(
+    original_df = pd.read_csv(pth)
+    original_df['Churn'] = original_df.Attrition_Flag.apply(
         lambda val: 0 if val == "Existing Customer" else 1)
-    return df
+    return original_df
 
 
 def perform_eda(df):
@@ -48,13 +46,12 @@ def perform_eda(df):
             None
     '''
     plt.figure(figsize=(20, 10))
-    #df.Churn.plot.hist().get_figure()
     df.Churn.plot.hist()
     plt.savefig('images/eda/Churn.png')
     plt.close()
-    
+
     plt.figure(figsize=(20, 10))
-    
+
     df.Customer_Age.plot.hist().get_figure().savefig('images/eda/Customer_Age.png')
     df.Marital_Status.value_counts('normalize').plot(
         kind='bar').figure.savefig('images/eda/Marital_Status.png')
@@ -64,8 +61,6 @@ def perform_eda(df):
         kde=True).figure.savefig('images/eda/Total_Trans_Ct.png')
     sns.heatmap(df.corr(), annot=False, cmap='Dark2_r',
                 linewidths=2).figure.savefig('images/eda/df_corr.png')
-   
-    return None
 
 
 def encoder_helper(df, category_lst, response):
@@ -76,24 +71,25 @@ def encoder_helper(df, category_lst, response):
     input:
             df: pandas dataframe
             category_lst: list of columns that contain categorical features
-            response: string of response name [optional argument that could be used for naming variables or index y column]
+            response: string of response name [optional argument that could be used for
+                                               naming variables or index y column]
 
     output:
             df: pandas dataframe with new columns for
     '''
     # gender encoded column
     y = response
-    
+
     for category in category_lst:
-            
+
         temp_lst = []
         temp_groups = df.groupby(category).mean()[y]
-    
+
         for val in df[category]:
             temp_lst.append(temp_groups.loc[val])
-    
+
         df['{0}_Churn'.format(category)] = temp_lst
-    
+
     return df
 
 
@@ -101,36 +97,50 @@ def perform_feature_engineering(df, response):
     '''
     input:
               df: pandas dataframe
-              response: string of response name [optional argument that could be used for naming variables or index y column]
+              response: string of response name [optional argument that could be used for
+                                                 naming variables or index y column]
 
     output:
-              X_train: X training data
-              X_test: X testing data
+              x_train: X training data
+              x_test: X testing data
               y_train: y training data
               y_test: y testing data
     '''
     cat_columns = [
-    'Gender',
-    'Education_Level',
-    'Marital_Status',
-    'Income_Category',
-    'Card_Category']
-    
+        'Gender',
+        'Education_Level',
+        'Marital_Status',
+        'Income_Category',
+        'Card_Category']
+
     df = encoder_helper(df, cat_columns, response)
     y = df[response]
-    keep_cols = ['Customer_Age', 'Dependent_count', 'Months_on_book',
-             'Total_Relationship_Count', 'Months_Inactive_12_mon',
-             'Contacts_Count_12_mon', 'Credit_Limit', 'Total_Revolving_Bal',
-             'Avg_Open_To_Buy', 'Total_Amt_Chng_Q4_Q1', 'Total_Trans_Amt',
-             'Total_Trans_Ct', 'Total_Ct_Chng_Q4_Q1', 'Avg_Utilization_Ratio',
-             'Gender_Churn', 'Education_Level_Churn', 'Marital_Status_Churn', 
-             'Income_Category_Churn', 'Card_Category_Churn']
+    keep_cols = [
+        'Customer_Age',
+        'Dependent_count',
+        'Months_on_book',
+        'Total_Relationship_Count',
+        'Months_Inactive_12_mon',
+        'Contacts_Count_12_mon',
+        'Credit_Limit',
+        'Total_Revolving_Bal',
+        'Avg_Open_To_Buy',
+        'Total_Amt_Chng_Q4_Q1',
+        'Total_Trans_Amt',
+        'Total_Trans_Ct',
+        'Total_Ct_Chng_Q4_Q1',
+        'Avg_Utilization_Ratio',
+        'Gender_Churn',
+        'Education_Level_Churn',
+        'Marital_Status_Churn',
+        'Income_Category_Churn',
+        'Card_Category_Churn']
     df = df[keep_cols]
     X = df
-    X_train, X_test, y_train, y_test = train_test_split(
+    x_train, x_test, y_train, y_test = train_test_split(
         X, y, test_size=0.3, random_state=42)
 
-    return X_train, X_test, y_train, y_test
+    return x_train, x_test, y_train, y_test
 
 
 def classification_report_image(y_train,
@@ -153,36 +163,16 @@ def classification_report_image(y_train,
     output:
              None
     '''
-    plt.clf()
-    plt.rc("figure", figsize=(5, 5))
-    plt.text(0.01,
-             1.50,
-             str(classification_report(y_test, y_test_preds_rf)),
-             {"fontsize": 10},
-             fontproperties="monospace")
-    
-    plt.text(0.01,
-             2.00,
-             str(classification_report(y_train, y_train_preds_rf)),
-             {"fontsize": 10},
-             fontproperties="monospace")
-    
-    plt.text(0.01,
-             3.00,
-             str(classification_report(y_test, y_test_preds_lr)),
-             {"fontsize": 10},
-             fontproperties="monospace")
-    
-    plt.text(0.01,
-             4.00,
-             str(classification_report(y_train, y_train_preds_lr)),
-             {"fontsize": 10},
-             fontproperties="monospace")
-    
+    fig = plt.figure(figsize=(10, 10))
+    ax = fig.add_subplot()
+    fig.subplots_adjust(top=0.85)
+    fig.suptitle('Classification Report', fontsize=14, fontweight='bold')
+    ax.axis([0, 20, 0, 20])
+    ax.text(0.1, 15, str(classification_report(y_test, y_test_preds_lr)))
+    ax.text(10, 15, str(classification_report(y_train, y_train_preds_rf)))
+    ax.text(0.1, 1, str(classification_report(y_test, y_test_preds_lr)))
+    ax.text(10, 1, str(classification_report(y_train, y_train_preds_lr)))
     plt.savefig("images/results/rf_test_cr_results.jpg")
-    plt.close()
-       
-    return None
 
 
 def feature_importance_plot(model, X_data, output_pth):
@@ -196,39 +186,36 @@ def feature_importance_plot(model, X_data, output_pth):
     output:
              None
     '''
-    
 
     importances = model.feature_importances_
     # Sort feature importances in descending order
     indices = np.argsort(importances)[::-1]
-    
+
     # Rearrange feature names so they match the sorted feature importances
     names = [X_data.columns[i] for i in indices]
-    
+
     # Create plot
-    plt.figure(figsize=(20,8))
-    
+    plt.figure(figsize=(20, 12))
+
     # Create plot title
     plt.title("Feature Importance")
     plt.ylabel('Importance')
-    
+
     # Add bars
     plt.bar(range(X_data.shape[1]), importances[indices])
-    
+
     # Add feature names as x-axis labels
     plt.xticks(range(X_data.shape[1]), names, rotation=90)
-    
-    plt.savefig(output_pth)
-    
-    return None
-    
 
-def train_models(X_train, X_test, y_train, y_test):
+    plt.savefig(output_pth)
+
+
+def train_models(x_train, x_test, y_train, y_test):
     '''
     train, store model results: images + scores, and store models
     input:
-              X_train: X training data
-              X_test: X testing data
+              x_train: X training data
+              x_test: X testing data
               y_train: y training data
               y_test: y testing data
     output:
@@ -249,15 +236,15 @@ def train_models(X_train, X_test, y_train, y_test):
     }
 
     cv_rfc = GridSearchCV(estimator=rfc, param_grid=param_grid, cv=5)
-    cv_rfc.fit(X_train, y_train)
+    cv_rfc.fit(x_train, y_train)
 
-    lrc.fit(X_train, y_train)
+    lrc.fit(x_train, y_train)
 
-    y_train_preds_rf = cv_rfc.best_estimator_.predict(X_train)
-    y_test_preds_rf = cv_rfc.best_estimator_.predict(X_test)
+    y_train_preds_rf = cv_rfc.best_estimator_.predict(x_train)
+    y_test_preds_rf = cv_rfc.best_estimator_.predict(x_test)
 
-    y_train_preds_lr = lrc.predict(X_train)
-    y_test_preds_lr = lrc.predict(X_test)
+    y_train_preds_lr = lrc.predict(x_train)
+    y_test_preds_lr = lrc.predict(x_test)
 
     classification_report_image(y_train,
                                 y_test,
@@ -265,31 +252,24 @@ def train_models(X_train, X_test, y_train, y_test):
                                 y_train_preds_rf,
                                 y_test_preds_lr,
                                 y_test_preds_rf)
-    
+
+    lrc_plot = plot_roc_curve(lrc, x_test, y_test)
+    plt.figure(figsize=(15, 8))
+    ax = plt.gca()
+    rfc_disp = plot_roc_curve(
+        cv_rfc.best_estimator_,
+        x_test,
+        y_test,
+        ax=ax,
+        alpha=0.8)
+    lrc_plot.plot(ax=ax, alpha=0.8)
+    plt.savefig("images/results/roc.jpg")
+
     # save best model
     joblib.dump(cv_rfc.best_estimator_, './models/rfc_model.pkl')
     joblib.dump(lrc, './models/logistic_model.pkl')
-    
-    feature_importance_plot(cv_rfc.best_estimator_, X_train, "images/results/feature_importance.jpg")
 
-    return None
-
-
-if __name__ == "__main__":
-
-    # STEP 1 - DONE
-    df = import_data("./data/bank_data.csv")
-
-    # STEP 2 - DONE
-    perform_eda(df)
-    
-    # STEP 3- DONE
-    X_train, X_test, y_train, y_test = perform_feature_engineering(df,'Churn')
-
-    # STEP 4 -
-    train_models(X_train, X_test, y_train, y_test)
-    
-    #model = joblib.load('./models/rfc_model.pkl')
-    
-#
-#    print(df.shape)
+    feature_importance_plot(
+        cv_rfc.best_estimator_,
+        x_train,
+        "images/results/feature_importance.jpg")
--- churn_script_logging_and_tests.py
+++ churn_script_logging_and_tests.py
@@ -6,67 +6,152 @@ Created on Sun May  1 10:47:22 2022
 """
 
 import os
+import pytest
+import glob
 import logging
-import churn_library_solution as cls
+import churn_library
 
 logging.basicConfig(
     filename='./logs/churn_library.log',
-    level = logging.INFO,
+    level=logging.INFO,
     filemode='w',
     format='%(name)s - %(levelname)s - %(message)s')
 
-def test_import(import_data):
-	'''
-	test data import - this example is completed for you to assist with the other test functions
-	'''
-	try:
-		df = import_data("./data/bank_data.csv")
-		logging.info("Testing import_data: SUCCESS")
-	except FileNotFoundError as err:
-		logging.error("Testing import_eda: The file wasn't found")
-		raise err
 
-	try:
-		assert df.shape[0] > 0
-		assert df.shape[1] > 0
-	except AssertionError as err:
-		logging.error("Testing import_data: The file doesn't appear to have rows and columns")
-		raise err
-
-
-def test_eda(perform_eda):
-	'''
-	test perform eda function
-	'''
+@pytest.fixture(name='df_original')
+def df_original_():
+    """
+    Original dataframe fixture - returns the original dataframe
+    """
     try:
-        
+        df_original = churn_library.import_data("./data/bank_data.csv")
+        logging.info("Original dataframe fixture: SUCCESS")
+    except FileNotFoundError as err:
+        logging.error("Original dataframe fixture: The file wasn't found")
+        raise err
+    return df_original
 
 
-def test_encoder_helper(encoder_helper):
-	'''
-	test encoder helper
-	'''
+@pytest.fixture(name='df_encoded')
+def df_encoded_(df_original):
+    """
+    Encoded dataframe fixture - returns the dataframe after Encoding of Categorical Features
+    """
+    try:
+        cat_columns = [
+            'Gender',
+            'Education_Level',
+            'Marital_Status',
+            'Income_Category',
+            'Card_Category']
+
+        response = 'Churn'
+
+        df_encoded = churn_library.encoder_helper(
+            df_original, cat_columns, response)
+
+        logging.info("Encoded dataframe fixture: SUCCESS")
+    except KeyError() as err:
+        logging.error(
+            "Encoded dataframe fixture: Required Features Not Present")
+        raise err
+    return df_encoded
 
 
-def test_perform_feature_engineering(perform_feature_engineering):
-	'''
-	test perform_feature_engineering
-	'''
+@pytest.fixture(name='df_processed')
+def df_processed_(df_original):
+    """
+    Processed dataframe fixture - returns the Train Test Split Processed Data
+    """
+    try:
+        x_train, x_test, y_train, y_test = churn_library.perform_feature_engineering(
+            df_original, 'Churn')
+        logging.info("Processed dataframe fixture: SUCCESS")
+    except KeyError() as err:
+        logging.error(
+            "Processed dataframe fixture: Feature Engineering Failed")
+        raise err
+    return x_train, x_test, y_train, y_test
 
 
-def test_train_models(train_models):
-	'''
-	test train_models
-	'''
+def test_import(df_original):
+    '''
+    test data import - test if orginal dataset loaded
+    '''
+    try:
+        assert not df_original.empty
+        logging.info("Testing import_data: SUCCESS")
+    except AssertionError as err:
+        logging.error("Testing import_data: The file wasn't found")
+        raise err
+
+    try:
+        assert df_original.shape[0] > 0
+        assert df_original.shape[1] > 0
+    except AssertionError as err:
+        logging.error(
+            "Testing import_data: The file doesn't appear to have rows and columns")
+        raise err
 
 
-if __name__ == "__main__":
-	pass
+def test_eda(df_original):
+    '''
+    test perform eda function
+    '''
+    try:
+        churn_library.perform_eda(df_original)
+        assert os.path.exists('./images/eda/Churn.png')
+        assert os.path.exists('./images/eda/Customer_Age.png')
+        assert os.path.exists('./images/eda/df_corr.png')
+        assert os.path.exists('./images/eda/Marital_Status.png')
+        assert os.path.exists('./images/eda/Total_Trans_Ct.png')
+    except AssertionError as err:
+        logging.error("Testing eda: Images Not Generated")
+        raise err
 
 
+def test_encoder_helper(df_encoded):
+    '''
+        test encoder helper
+    '''
+    try:
+        assert not df_encoded.empty
+    except AssertionError as err:
+        logging.error("Testing Encoder Helper: Required Features Not Present")
+        raise err
 
 
+def test_perform_feature_engineering(df_processed):
+    '''
+        test perform_feature_engineering
+    '''
+    try:
+        x_train, x_test, y_train, y_test = df_processed
+        assert x_train.shape[0] == len(y_train)
+        assert x_test.shape[0] == len(y_test)
+    except AssertionError as err:
+        logging.error(
+            "Testing Feature Engineering: Feature Engineering Failed ")
+        raise err
 
 
+def test_train_models(df_processed):
+    '''
+        test train_models
+    '''
+    try:
+        x_train, x_test, y_train, y_test = df_processed
+        churn_library.train_models(x_train, x_test, y_train, y_test)
+        assert os.path.exists('./models/logistic_model.pkl')
+        assert os.path.exists('./models/rfc_model.pkl')
+    except AssertionError as err:
+        logging.error("Testing Models: Trained Models Not Available ")
+        raise err
 
 
+# if __name__ == "__main__":
+#     for directory in ["./images/eda", "./images/results", "./models"]:
+#         files = glob.glob("%s/*" % directory)
+#         for file in files:
+#             os.remove(file)
+#     pytest.main(["-s"])
